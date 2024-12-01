f93b7096108c3270f79798895a0dbc01b10e8281
DewinU <umana.dewin@gmail.com>
2024-12-01 11:33:04 -0600

    Refactor routing: integrate router setup into main.tsx and remove router.tsx; add react-query-devtools dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { NextUIProvider } from '@nextui-org/react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
+import { ErrorPage } from './ErrorPage.tsx';
+import {
+  StoreLayout,
+  CompleteListPage,
+  MensPage,
+  WomensPage,
+  NewProduct,
+} from './products';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <StoreLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '',
+        element: <CompleteListPage />,
+      },
+      {
+        path: 'men',
+        element: <MensPage />,
+      },
+      {
+        path: 'women',
+        element: <WomensPage />,
+      },
+      {
+        path: 'new',
+        element: <NewProduct />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
